25a1ed3e3959b1f687f281a33c0ed47706df03e0
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-10-19 22:11:28 -0400

    Fase 1.2 La suma bien colocada.

--- src/Elementos_Básicos.java
+++ src/Elementos_Básicos.java
@@ -3,7 +3,7 @@ import java.awt.event.* ;
 import java.awt.* ;
 // implements ActionListener
 public class Elementos_Básicos extends JFrame {
-
+    // ⊕
     private JButton suma, resta, multiplicación, división;
     //private JLabel ;
     //private JTextField ;
@@ -15,15 +15,15 @@ public class Elementos_Básicos extends JFrame {
         getContentPane().setBackground(new Color(0,255,0));
 
         suma = new JButton("+");
-        suma.setBounds(20,150,100,30);
-        suma.setBackground(new Color(255,255,255));
+        suma.setBounds(20,20,50,50);
+        suma.setBackground(new Color(0,0,0));
         suma.setFont(new Font("Andale Mono", 1, 14));
-        suma.setForeground(new Color(255,0,0));
+        suma.setForeground(new Color(255,255,255));
         //suma.addActionListener(this);
         add(suma);
 
         resta = new JButton("-");
-        resta.setBounds(20,200,100,30);
+        resta.setBounds(10,150,50,30);
         resta.setBackground(new Color(255,255,255));
         resta.setFont(new Font("Andale Mono", 1, 14));
         resta.setForeground(new Color(255,0,0));
@@ -31,7 +31,7 @@ public class Elementos_Básicos extends JFrame {
         add(resta);
 
         multiplicación = new JButton("*");
-        multiplicación.setBounds(25,50,100,30);
+        multiplicación.setBounds(10,200,50,30);
         multiplicación.setBackground(new Color(255,255,255));
         multiplicación.setFont(new Font("Andale Mono", 1, 14));
         multiplicación.setForeground(new Color(255,0,0));
@@ -39,7 +39,7 @@ public class Elementos_Básicos extends JFrame {
         add(multiplicación);
 
         división = new JButton("/");
-        división.setBounds(25,280,100,30);
+        división.setBounds(20,280,100,30);
         división.setBackground(new Color(255,255,255));
         división.setFont(new Font("Andale Mono", 1, 14));
         división.setForeground(new Color(255,0,0));
