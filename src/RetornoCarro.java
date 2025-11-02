1fabcf2fc6aa3f827dbd61fb223594abdef94c38
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-11-02 13:05:21 -0500

    Bastante avance. Aunque falta.

--- src/RetornoCarro.java
+++ src/RetornoCarro.java
@@ -4,8 +4,7 @@ public class RetornoCarro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double a = 0.6*0.4;
-        System.out.println(a);
+
 
         scanner.close();
     }
