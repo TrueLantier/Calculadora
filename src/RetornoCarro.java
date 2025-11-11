7e768f4aa7a24a55294e7fae917dc473f2c8b735
Angel Eduardo Pedraza Ordoñez <leedgne@gmail.com>
2025-11-11 16:50:07 -0500

    Números negativos introducidos.

--- src/RetornoCarro.java
+++ src/RetornoCarro.java
@@ -5,10 +5,7 @@ public class RetornoCarro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       double a = 2.7;
-       if ( (int) a < a ) {
-           System.out.println("Funciona");
-       }
+
 
         int num[] = new int[10];
 
